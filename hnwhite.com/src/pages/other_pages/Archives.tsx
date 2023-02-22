ef1d61fe7894563b1c3f1ca27b54c9c13a3454cd
c3charvat <c3charvat@gmail.com>
2023-02-21 21:17:46 -0500

    background_img

--- hnwhite.com/src/pages/other_pages/Archives.tsx
+++ hnwhite.com/src/pages/other_pages/Archives.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { Typography, Box } from "@mui/material";
+import { Typography, Box,Paper } from "@mui/material";
+var logo= require('./Static_Img/Background_V1.gif');
 
 function apples() {
   return 0;
@@ -8,8 +9,8 @@ function apples() {
 export default function HnwStoryContent() {
   return (
     <React.Fragment>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 , paddingTop:'80px'}}> 
-        <Typography paragraph>Archives</Typography>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 , paddingTop:'80px',backgroundImage:`url(${logo})`,width: '100vw', height: '100vh'}}> 
+        <Typography paragraph>Archive</Typography>
         <Typography paragraph>Content 1</Typography>
       </Box>
     </React.Fragment>
--- hnwhite.com/src/pages/other_pages/Collection.tsx
+++ hnwhite.com/src/pages/other_pages/Collection.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Typography, Box } from "@mui/material";
+var logo= require('./Static_Img/Background_V1.gif');
 
 function apples() {
   return 0;
@@ -8,7 +9,7 @@ function apples() {
 export default function CollectionContent() {
   return (
     <React.Fragment>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 , paddingTop:'80px'}}> 
+      <Box component="main" sx={{ flexGrow: 1, p: 3 , paddingTop:'80px',backgroundImage:`url(${logo})`,width: '100vw', height: '100vh'}}> 
         <Typography paragraph>Collection.</Typography>
         <Typography paragraph>Content 1</Typography>
       </Box>
